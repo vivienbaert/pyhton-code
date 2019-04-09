3b6927a6dcf4c13b539fffd80a2d6be8170765bb
vivienbaert <36931910+vivienbaert@users.noreply.github.com>
2019-04-09 11:26:13 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 x=np.linspace(-10,10,100)
-plt.plot(x,np.sin(x))  # on utilise la fonction sinus de Numpy
+plt.plot(x,np.sin(2*x))  # on utilise la fonction sinus de Numpy
 plt.ylabel('fonction sinus')
 plt.xlabel("l'axe des abcisses")
 plt.show()
